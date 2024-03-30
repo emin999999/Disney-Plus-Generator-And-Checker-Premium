83bd2bdcaca9333b250b0962534c872adb4bd72e
Emin <163546443+emin999999@users.noreply.github.com>
2024-03-30 09:23:27 +0300

    Update disney plus checker and generator premium.py

--- disney plus checker and generator premium.py
+++ disney plus checker and generator premium.py
@@ -1,5 +1,5 @@
 print("Owned By Emin INC.")
-print("Disney Plus Account Generator And Checker 1.0")
+print("Disney Plus Account Premium Edition Generator And Checker 1.0")
 import random
 import string
 import time
